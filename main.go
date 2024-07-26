d5d31c969818b50422bfc2cd99824b8f0af33da5
Maziar Sojoudian <maziar@sojoudian.net>
2024-07-26 13:31:19 -0400

    update go app

--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
-	appColor := os.Getenv("APP_COLOR")
-	appMode := os.Getenv("APP_MODE")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		appColor := os.Getenv("APP_COLOR")
+		appMode := os.Getenv("APP_MODE")
 
-	fmt.Printf("App color: %s\n", appColor)
-	fmt.Printf("App mode: %s\n", appMode)
+		response := fmt.Sprintf("App color: %s\nApp mode: %s\n", appColor, appMode)
+		fmt.Fprint(w, response)
+	})
+
+	port := "8080"
+	fmt.Printf("Server starting on port %s\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		fmt.Printf("Failed to start server: %v\n", err)
+	}
 }
